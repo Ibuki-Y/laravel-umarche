f68afc6919a5724cdd93895e5ecf5b457bc2fde7
Ibuki-Y <ibu.love.drum@gmail.com>
2022-08-09 22:12:35 +0900

    Use component attribute

--- resources/views/tests/component-1.blade.php
+++ resources/views/tests/component-1.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
   <x-slot name="header">Header</x-slot>
   test1
+
+  <x-tests.card title="Title" content="Content" />
 </x-tests.app>
--- resources/views/tests/component-2.blade.php
+++ resources/views/tests/component-2.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
   <x-slot name="header">Header</x-slot>
   test2
+
+  <x-tests.card title="Title" content="Content" />
 </x-tests.app>
