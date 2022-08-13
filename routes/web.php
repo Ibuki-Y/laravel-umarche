a7872b3e047625d1c278b1abb65518ccf9c114eb
Ibuki-Y <ibu.love.drum@gmail.com>
2022-08-13 15:55:03 +0900

    Add multi login

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use App\Http\Controllers\LifeCycleTestController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('user.dashboard');
+})->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-2', [ComponentTestController::class, 'showComponent2']);
