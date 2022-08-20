78a8c65abd4cf5c0ddcf79d9b4c5e80a63ac5bd1
Ibuki-Y <ibu.love.drum@gmail.com>
2022-08-20 22:40:31 +0900

    Add middleware setting

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use App\Models\Shop;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShopController extends Controller {
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:owners');
+        $this->middleware(function ($request, $next) {
+            // dd($request->route()->parameter('shop')); // 文字列
+            // dd(Auth::id()); // 数字
+            $id = $request->route()->parameter('shop');
+            if (!is_null($id)) {
+                $shopOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopOwnerId;
+                $ownerId = Auth::id();
+                if ($shopId !== $ownerId) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index', compact('shops'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+    }
+}
