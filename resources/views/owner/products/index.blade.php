d0df8f22e48fbeeffc3bc1e2de0a4b9e1883d0c5
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-01 16:43:05 +0900

    Add Product store

--- resources/views/owner/products/index.blade.php
+++ resources/views/owner/products/index.blade.php
@@ -20,7 +20,7 @@
                 <div class="w-1/4 p-2 md:p-4 hover:opacity-90">
                   <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}">
                     <div class="border rounded-md p-2 md:p-4">
-                      <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
+                      <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
                       <div class="text-gray-600">{{ $product->name }}</div>
                     </div>
                   </a>
