77a19852ef7869907ae70e236ebcbdb85005a3f9
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-11 00:54:23 +0900

    Add cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,15 +4,32 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller {
     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach ($products as $product) {
+            $totalPrice = $product->price * $product->pivot->quantity;
+        }
+
+        return view('user.cart.index', compact('products', 'totalPrice'));
+    }
+
+    /**
      * Add the specified resource in storage.
      *
      * @param \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function add(Request $request) {
@@ -31,6 +48,6 @@ class CartController extends Controller {
             ]);
         }
 
-        dd('passed');
+        return redirect()->route('user.cart.index');
     }
 }
