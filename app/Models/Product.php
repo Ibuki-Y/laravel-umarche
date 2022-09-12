a868e956c7e5c4274696d6981e4af302f3619180
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-12 14:15:02 +0900

    Add product local scope

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,8 @@ use App\Models\Stock;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model {
     use HasFactory;
@@ -58,5 +60,38 @@ class Product extends Model {
     public function users() {
         return $this->belongsToMany(User::class, 'carts')
             ->withPivot(['id', 'quantity']);
+    }
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function scopeAvailableItems(Builder $builder) {
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) as quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+        return $builder
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id as id',
+                'products.name as name',
+                'products.price',
+                'products.sort_order as sort_order',
+                'products.information',
+                'secondary_categories.name as category',
+                'image1.filename as filename'
+            );
     }
 }
