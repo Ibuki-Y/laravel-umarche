67d6b526b553ab9e0e061b1badbc25846791d0c4
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-14 00:41:04 +0900

    Add CartService

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Stock;
 use App\Models\User;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -72,6 +73,11 @@ class CartController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function checkout() {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        ////
+
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
         $lineItems = [];
